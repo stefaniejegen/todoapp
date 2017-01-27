89bd4fd02bce6826b557a38c6b057fe2c3d0ee1b
Stefanie Jegen <jegenstefanie@gmail.com>
2017-01-27 16:42:18 +0100

    First Page html/css

---
+++ Gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
+
+gulp.task('styles', function(){
+    gulp.src('scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./scss/orig/'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./scss/'));
+});
+
+gulp.task('default', function () {
+    browserSync.init({
+        server: './'
+    });
+
+    gulp.watch('scss/*.scss', ['styles']);
+});
