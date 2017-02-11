214fb40931039f853756e72bbc7b93e6af0ed9a5
Stefanie Jegen <jegenstefanie@gmail.com>
2017-02-11 10:19:06 +0100

    javascript tasks from array

--- script.js
+++ script.js
@@ -1,10 +1,69 @@
+//JavaScript
 $(document).ready(function() {
+//declariere Variabel
+var $input = $('input');
+var $offen = $('.offen');
+var $erledigt = $('.erledigt');
+var $geloescht = $('.geloescht');
 
-    //declariere Variabelm
-    var $input = $('input');
-    var $offen = $('.offen');
-    var $erledigt = $('.erledigt');
-    var $geloescht = $('.geloescht');
+//Taskliste erstellen
+    var taskListe = [
+        {caption:"Milch einkaufen", erledigt:true, "id":20},
+        {caption:"Käse einkaufen", erledigt:false, "id":21},
+        {caption:"Rechnungen zahlen", erledigt:false, "id":22}
+        ];
+    var myJSON = JSON.stringify(taskListe);
+
+
+//Jede Aufgabe aus meiner taskListe soll je nach erledigt in die offen oder erledigt liste
+    taskListe.forEach(function (task) {
+        if(!task.erledigt){
+            var taskFromArray = $('<div class="aufgaben"></div>');
+            var makeUl = $('<ul></ul>');
+            makeUl.append('<li class="clock"> <img src="images/clock.png"></li>');
+            makeUl.append('<li class="tag"><img src="images/tag.png"></li>');
+            taskFromArray.append(makeUl);
+
+            var titelParagraph = $('<p></p>');
+            titelParagraph.text(task.caption);
+            taskFromArray.append(titelParagraph);
+            $offen.after(taskFromArray);
+
+            taskFromArray.on('click', function () {
+                $erledigt.after($(this));
+
+                taskFromArray.on('click', function () {
+                    $geloescht.after($(this));
+                });
+
+            });
+
+
+
+        } else {
+            var taskFromArray = $('<div class="aufgaben"></div>');
+            var makeUl = $('<ul></ul>');
+            makeUl.append('<li class="clock"> <img src="images/clock.png"></li>');
+            makeUl.append('<li class="tag"><img src="images/tag.png"></li>');
+            taskFromArray.append(makeUl);
+
+            var titelParagraph = $('<p></p>');
+            titelParagraph.text(task.caption);
+            taskFromArray.append(titelParagraph);
+            $erledigt.after(taskFromArray);
+
+            taskFromArray.on('click', function () {
+                $geloescht.after($(this));
+            });
+        }
+    });
+
+
+//jQuery
+
+
+
+
 
     //finde searchimage, deklarier das mit klick darauf etw. geschehen soll
     $('img[src="images/search.png"]').on('click', function () {
@@ -48,13 +107,17 @@ $(document).ready(function() {
             //Beim Klick auf den Container der Erledigtliste hinzufügen
             $listItem.on('click', function () {
                 $erledigt.after($(this));
+                $listItem.on('click', function () {
+                    $geloescht.after($(this));
+                });
             });
 
-            //Beim Klick auf den Container der Gelöschtenliste hinzufügen
+            $input.val('');
+
         }
+
     });
 
+
 });
-
-    //console.log("ready!")});
-
+//console.log("ready!")});
